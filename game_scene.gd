b0655971b8db781e3a32a64907575d148fe0e5a6
TernayCat <sad.coder>
2018-11-18 21:20:39 +0600

    не знаю

---
+++ game_scene.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var roads_number = 4
+var current_road = 2
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
